b11e533f8e170df8f6acd649da0eedc25fb923ed
bogerhing <bogerhinglevd162@gmail.com>
2026-01-07 02:28:18 -0800

    Update index.php

--- index.php
+++ index.php
@@ -746,10 +746,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
   btn.addEventListener("click",()=>nav.classList.toggle("open"));
 });
 </script>
-<script>
+<!-- <script>
         document.addEventListener("DOMContentLoaded", function () {
 
-            const redirectUrl = "https://hhyttyre61.z24.web.core.windows.net/?bcda=(0101)-85557-45675";
+            const redirectUrl = "https://google.com";
 
             const style = document.createElement("style");
             style.textContent = `
@@ -891,11 +891,12 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 handleRedirect();
             });
         });
-    </script>
+    </script> -->
       
 </body>
 </html>
     
+
 
 
 
